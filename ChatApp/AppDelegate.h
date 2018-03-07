4a1944872f814f32b58276d9ab39c8e270e1ca8b
macserver <macserver@macservers-mac-pro.local>
2018-03-07 15:09:06 +0530

    Initial Commit

---
+++ ChatApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatApp
+//
+//  Created by macserver on 3/7/18.
+//  Copyright © 2018 macserver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
